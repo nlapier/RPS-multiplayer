1e3a252c145f6f454714efc7292ae2fc1ffeede2
Nathaniel LaPier <Nat@Nathaniels-Air.home>
2016-06-26 16:31:24 -0400

    Initial site files added

---
+++ Assets/javascript/game.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCICumjmJWEeFOlY39Zh-8zzHFY-xta8nY",
+    authDomain: "rps-multiplayer-40946.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-40946.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
